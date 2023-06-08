e8db9f48d792d2b2809dd8335fbb7dd0ec036cde
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-06-08 13:57:22 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+<h1>{{title}}</h1>
+  <ButtonStandard @click="incremen">Click me</ButtonStandard>
+
+</template>
+
+<script>
+
+import ButtonStandard from "./components/ButtonStandard.vue";
+
+export default {
+  name: "App",
+  components: {
+    ButtonStandard,
+
+  },
+  data(){
+    return{
+        amountOfClicks: 0
+    }
+    },
+    computed :{
+      title(){
+        return `Amount ot clicks ${this.amountOfClicks}`
+      }
+},
+methods:{
+  incremen(){
+    this.amountOfClicks += 1
+  }
+}}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<!-- npm run serve -->
---
+++ src/components/ButtonStandard.vue
@@ -0,0 +1,19 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonStandard",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
